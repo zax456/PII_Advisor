543a46f425df1696395fc486f14db527fe50f1c2
tonytonggg <tonytongwa@gmail.com>
2019-09-24 00:48:19 +0800

    making changes from team call 24th sep

--- Software_Engineering/convert_to_text.py
+++ Software_Engineering/convert_to_text.py
@@ -1,21 +1,47 @@
 import unittest
 import re
+import os
+import docx2txt
+from odf import text, teletype
+from odf.opendocument import load
 
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
 from io import StringIO
-import os
 
+#remaining to test: doc, txt
 
 def convert_to_text(filepath):
+    if filepath[-3:]=='pdf':
+        return getPDFcontent(filepath)
+    if filepath[-3:]=='odt':
+        return getODTContent(filepath)
+    if filepath[-3:]=='doc':
+        return getDOCcontent(filepath)
+    if filepath[-3:]=='txt':
+        return getTXTcontent(filepath)
+    if filepath[-4:]=='docx':
+        return getDocxContent(filepath)
+
+
+def getDOCcontent(filepath):    #couldnt find a way for Windows so i converted the file, but for mac there is antiword/textract     
+    docx_file = '{0}{1}'.format(filepath, 'x')
+    content = getDocxContent(docx_file)
+    return content
+
+def getTXTcontent(filepath):
+    content = open(filepath, "r")
+    return content
+
+def getPDFcontent(filepath):
     """
     Function:
-	Takes in a file and converts contents to python strings. 
-	
+    Takes in a file and converts contents to python strings. 
+    
     Input:
-	filepath: 1 file of (PDF/ doc format) in a directory
+    filepath: 1 file of (PDF/ doc format) in a directory
 
     Output:
     A giant string (in UTF-8 format) containing all the text found, in “sequential” order (Text should be split by 1 whitespace)
@@ -24,7 +50,7 @@ def convert_to_text(filepath):
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
     codec = 'utf-8'
-    laparams = LAParams()
+    laparams = LAParams(line_margin=0.1)
     device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
     fp = open(filepath, 'rb')
     interpreter = PDFPageInterpreter(rsrcmgr, device)
@@ -42,6 +68,18 @@ def convert_to_text(filepath):
     
     return text
 
+def getDocxContent(filename):
+    DocxText = docx2txt.process(filename)
+    return DocxText
+
+def getODTContent(filename):
+    list=[]
+    textdoc = load(filename)
+    allparas = textdoc.getElementsByType(text.P)
+    for i in range(len(allparas)):
+        list.append(teletype.extractText(allparas[i]))
+    return ' '.join(list)
+
 def convert_to_text_dir(dir, directory = True):
     """
     Takes in a directory and outputs all the strings found while processing the contents of each resume.
@@ -54,39 +92,3 @@ def convert_to_text_dir(dir, directory = True):
     """    
     return ["",""]
 
-###################################################################
-
-directory = os.getcwd()
-directory
-
-def contains_word(s, w):
-    return f' {w} ' in f' {s} '
-
-class Test(unittest.TestCase):
-
-    def test_1(self):
-        actual = convert_to_text(directory + '/sample_resumes/' + "kh_resume.pdf")
-#        this is 1 test case that will pass the test case
-#        actual = "Ang Kian Hwee Blk123 Choa Chu Kang Loop #02-34 S680341 Email: angkianhwee@u.nus.edu EDUCATION \
-#        National University of Singapore (NUS) Bachelor of Science (Business Analytics), Honours \
-#        Aug 2016 – present 25 years old NRIC: S1234567A Relevant Coursework: Data Management and Chinese, \
-#        Business and Technical Communication, Application Systems Development for Business Analytics, Regression Analysis,\
-#        Data Structure & Algorithms (Python, Java), Mining Web Data for Business Insights, Operations Research, Capstone Project,\
-#        Computational Methods for BA Expected Date of Graduation: December 2019"
-        actual = actual.lower()
-        found = True
-        phrases = ['ang kian hwee', 'blk123 choa chu kang loop #02-34 s680341', 'email: angkianhwee@u.nus.edu', 'education',
-           'national university of singapore (nus)', 'bachelor of science (business analytics), honours', 'aug 2016 – present',
-           '25 years old', 'nric: s1234567a', 'relevant coursework: data management and chinese,', 'business and technical communication,',
-           'application systems development for business analytics,', 'regression analysis,', 'data structure & algorithms (python, java),',
-           'mining web data for business insights, operations research,', 'capstone project,', 
-           'computational methods for ba', 'expected date of graduation: december 2019']
-        for p in phrases:
-            print(p)
-            found = contains_word(actual, p) and found
-            
-            if not found:
-                break
-        self.assertTrue(found)
-        
-# unittest.main(verbosity=2)
--- Software_Engineering/process_string.py
+++ Software_Engineering/process_string.py
@@ -1,7 +1,8 @@
 import unittest
-
+import phonenumbers
 import re
-
+from textblob import TextBlob
+import spacy
 #from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 #from pdfminer.converter import TextConverter
 #from pdfminer.layout import LAParams
@@ -10,23 +11,26 @@ import re
 
 def process_string(raw_text):
     """
-	Takes in a string and outputs a parsed string. Calls flagging() and parsing() functions to identify PIIs and mask the data respectively. 
+    Takes in a string and outputs a parsed string. Calls flagging() and parsing() functions to identify PIIs and mask the data respectively. 
     
     The raw string, Output from flagging(), Output from parsing() will be upsert into 3 different columns (Raw Contents, PIIs, Parsed Contents)
     for the record with the same uuid in database.
 
-	Input:
-	  text: String output from convert_to_text()
+    Input:
+      text: String output from convert_to_text()
 
-	Main Output:
+    Main Output:
       Returns HTTP status code 201 and indication msg to signify processing was done successfully. Upserts the values into database.
 
       Secondary (Indirect) Output:
       The raw resume string, Output from flagging(), Output from parsing() will be upsert into 3 different columns for the record with the same uuid in database. 
-	"""
-    PIIs = flagging(raw_text)
-    parse_text = parsing(raw_text, PIIs)
-    return PIIs, parse_text # for testing purposes only...remove this after confirming process_string works
+    """
+    try:
+        PIIs = flagging(raw_text)
+        parse_text = parsing(raw_text, PIIs)
+        return PIIs, parse_text
+    except:
+        print("process_string returned an error")
     # return ("Successfully processed and uploaded resume into database!")
 
 def flagging(raw_text):
@@ -36,10 +40,37 @@ def flagging(raw_text):
       Input: Output from convert_to_text()
 
       Output: returns an array of unique PIIs / returns a dictionary of lists of PIIs (there might be more than 1 value for 1 key, for exmaple giving home + personal phone number)
-	"""
-    nric = re.findall('(?i)[SFTG]\d{7}[A-Z]', raw_text)
-    email_address = re.findall("([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)", raw_text)
-    return {'email': [email_address], 'nric': [nric]}
+    """
+    try:
+        print(raw_text)
+        nric = re.findall('(?i)[SFTG]\d{7}[A-Z]', raw_text)
+        email_address = re.findall("([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)", raw_text)
+        phone_number = [phonenumbers.format_number(match.number, phonenumbers.PhoneNumberFormat.E164) for match in phonenumbers.PhoneNumberMatcher(raw_text, "SG")] 
+        search_physical_address = re.search('([A-Za-z]{2,6}[\s]?|)[\d]{1,4}' + # block/street number (e.g. Blk 123, Street 52, 133, St 55)
+                                            '[\s]?' +
+                                            '[A-Za-z]{,2}' + # block suffix (e.g. the 'A' in Block 60A)
+                                            '[\s]' +
+                                            '[\D]{5,}.+' + # road/street name (e.g. Kent Ridge Drive)
+                                            '[Ss][A-Za-z]{,8}[\s]?[\d]{6}' # postal code (e.g. S123456, Singapore 123456)
+                                            , raw_text)
+        physical_address = search_physical_address.group() if search_physical_address is not None else ''
+        name = process_name(raw_text).strip() if type(raw_text) == str else ''
+        return {'nric':nric,
+                'email':email_address,
+                'phone':phone_number,
+                'address':[physical_address] if physical_address else [],
+                'name':[name] if name else []
+                }
+    except Exception as e: 
+        print(e)
+        print("flagging returned an error")
+        # print({'nric':nric,
+        #         'email':email_address,
+        #         'phone':phone_number,
+        #         'address':[physical_address] if physical_address else [],
+        #         'name':[name] if name else []
+        #         })
+
 
 def parsing(raw_text, dic):
     """
@@ -52,74 +83,48 @@ def parsing(raw_text, dic):
       Output: 
       Entire string from convert_to_text() <pii: nric> labels over sensitive information.
     """
-    processed_text = raw_text
-    # Removing hard PIIs by default: NRIC, email address, phone, physical address
-    processed_text = re.sub("([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)", '<pii_email>', processed_text)
-    processed_text = re.sub('(?i)[SFTG]\d{7}[A-Z]', '<pii_nric>', processed_text)
-    # if PIIs then remove 
-    return processed_text
+    try:
+        processed_text = raw_text
+        # Removing hard PIIs by default: NRIC, email address, phone, physical address
+        processed_text = re.sub("([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)", '<pii_email>', processed_text)
+        processed_text = re.sub('(?i)[SFTG]\d{7}[A-Z]', '<pii_nric>', processed_text)
+        phone_number_raw = [match.raw_string for match in phonenumbers.PhoneNumberMatcher(processed_text, "SG")] 
+        for num in phone_number_raw:
+            processed_text=processed_text.replace(num, "<pii_phone>")
+        processed_text = re.sub('([A-Za-z]{2,6}[\s]?|)[\d]{1,4}' + # block/street number (e.g. Blk 123, Street 52, 133, St 55)
+                                '[\s]?' +
+                                '[A-Za-z]{,2}' + # block suffix (e.g. the 'A' in Block 60A)
+                                '[\s]' +
+                                '[\D]{5,}.+' + # road/street name (e.g. Kent Ridge Drive)
+                                '[Ss][A-Za-z]{,8}[\s]?[\d]{6}' # postal code (e.g. S123456, Singapore 123456)
+                                , '<pii_address>'
+                                , processed_text)
+        if dic['name'] != []:
+            processed_text = re.sub(dic['name'][0], '<pii_name>', processed_text)
+        processed_text = " ".join(processed_text.split())
+        processed_text = processed_text.replace("#", '') # remove redundant characters
+        # if PIIs then remove 
+        return processed_text
+    except Exception as e: 
+        print(e)
+        print("parsing returned an error")
+        # print(processed_text)
 
-###################################################################
+def process_name(raw_text):
+    """
+      Sub-function within flagging. Identifies Name by extracting the first string of noun/ "JJ" included for Indian Names
 
-class Test(unittest.TestCase):
-    
-    def test_1(self):
-        raw_text = "Ang Kian Hwee Blk123 Choa Chu Kang Loop #02-34 S680341 \
-        Email: angkianhwee@u.nus.edu EDUCATION \
-        National University of Singapore (NUS)"
-        dic, parsed_string = process_string(raw_text)
-        
-        # match each key to each value
-        test_dic = {'name': ['Ang Kian Hwee'], 'address': ['Blk123 Choa Chu Kang Loop #02-34 S680341'], 
-                    'email': ['angkianhwee@u.nus.edu']}
-        dic_check = True
-        pii_keys = ['name', 'address', 'email', 'nric', 'phone']
-        for key in pii_keys:
-            if dic[key] != test_dic[key]:
-                dic_check = False
-                break
-            
-        test_string = "<pii_name> <pii_address> Email: <pii_email> EDUCATION National University of Singapore (NUS)"
-        if parsed_string == test_string:
-            parsed_string_check = True
-        else:
-            parsed_string_check = False
-        self.assertTrue(parsed_string_check and dic_check)
-        
-        
-        
-    def test_2(self):
-        raw_text = "ALICE TAN MING NI S4598004D 16 JIAK CHUAN RD, SINGAPORE 089267 \
-        +65 9722 4728 FUNNYGIRL111@AOL.COM HTTPS://LINKEDIN.COM/ALICEELIOT Summary\
-        Experienced Server bringing enthusiasm, dedication and an exceptional work ethic.\
-        Trained in customer service with knowledge of Italy cuisine. High energy and outgoing\
-        with a dedication to positive guest relations. High volume dining customer service, and\
-        cash handling background."
-        dic, parsed_string = process_string(raw_text)
-        
-        # match each key to each value
-        test_dic = {'name': ['ALICE TAN MING NI'], 'nric': ['S4598004D'], 'address': ['16 JIAK CHUAN RD, SINGAPORE 089267'], 
-                    'email': ['angkianhwee@u.nus.edu'], 'phone': ['+65 9722 4728']}
-        dic_check = True
-        pii_keys = ['name', 'address', 'email', 'nric', 'phone']
-        for key in pii_keys:
-            if dic[key] != test_dic[key]:
-                dic_check = False
-                break
-            
-        test_string = "<pii_name> <pii_nric>  <pii_address> \
-        <pii_phone> <pii_email> HTTPS://LINKEDIN.COM/ALICEELIOT Summary\
-        Experienced Server bringing enthusiasm, dedication and an exceptional work ethic.\
-        Trained in customer service with knowledge of Italy cuisine. High energy and outgoing\
-        with a dedication to positive guest relations. High volume dining customer service, and\
-        cash handling background."
-        if parsed_string == test_string:
-            parsed_string_check = True
-        else:
-            parsed_string_check = False
-        self.assertTrue(parsed_string_check and dic_check)
-   
-# run this line
-# unittest.main(verbosity=2)
+      Input:
+          raw_text: Output from convert_to_text(), String Type
 
-        
+      Output: 
+      Name of string type
+      """
+    try:
+        nlp = spacy.load("model_building/model")
+        doc = nlp(raw_text)
+        for ent in doc.ents:  
+            if ent.label_ == "NAME":
+                return ent.text
+    except:
+        print("process_name returned an error") 
