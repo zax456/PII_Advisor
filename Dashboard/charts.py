23ccf2091ebe0f8e831f2d0a596162631d04d544
Ang Kian Hwee <kianhwee1994@hotmail.com>
2019-10-24 16:15:26 +0800

    Updated data_processing with functions to get data

---
+++ Dashboard/charts.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import data_processing as dp
+import plotly.graph_objs as go
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+chart1_df = dp.gen_chart_1()
+colors = list(np.random.choice(range(256), size=chart1_df.shape[0]))
+colors = list(np.random.choice(range(256), size=3))
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_ebola.csv')
+df = df.dropna(axis=0)
+
+layout = html.Div([
+    html.Div([html.H1("Ebola Cases Reported in Africa - 2014")], style={"textAlign": "center"}),
+    dcc.Graph(id="my-graph"),
+    html.Div([dcc.Slider(id='month-selected', min=3, max=12, value=8,
+                         marks={3: "March", 4: "April", 5: "May", 6: "June", 7: "July", 8: "August", 9: "September",
+                                10: "October", 11: "November", 12: "December"})],
+             style={'textAlign': "center", "margin": "30px", "padding": "10px", "width": "65%", "margin-left": "auto",
+                    "margin-right": "auto"}),
+], className="container")
+
+
+@app.callback(
+    dash.dependencies.Output("my-graph", "figure"),
+    [dash.dependencies.Input("month-selected", "value")]
+)
+def update_graph():
+    return {
+        "data": [go.Pie(labels=df["Country"].unique().tolist(), values=df[df["Month"] == selected]["Value"].tolist(),
+                        marker={'colors': ['#EF963B', '#C93277', '#349600', '#EF533B', '#57D4F1']}, textinfo='label')],
+        "layout": go.Layout(title=f"Cases Reported Monthly", margin={"l": 300, "r": 300, },
+                            legend={"x": 1, "y": 0.7})}
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
