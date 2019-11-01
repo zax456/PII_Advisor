87a48b5fcdd247e3cd571a0aa6cb269b95eaea82
Chen Yuan <leechenyuan.95@gmail.com>
2019-11-01 20:56:02 +0800

    debug

--- data_engineering/skills_extraction.py
+++ data_engineering/skills_extraction.py
@@ -2,7 +2,7 @@ import re
 
 ddir = '.' # working dir
 
-with open(f'{ddir}/skills.txt') as f:
+with open(f'{ddir}/skills.txt', encoding='utf8') as f:
     skills = f.readlines()
 
 skills = [skill.strip().lower() for skill in skills]
