57da8dafa5ba985d04899718e68988b248aa6d9c
Ang Kian Hwee <kianhwee1994@hotmail.com>
2019-11-10 14:47:24 +0800

    Housekeeping duties~
    - deleted previous versions of chart.py and data_processing
    - updated file names to original convention

--- Dashboard/assets/custom-script.js
+++ Dashboard/assets/custom-script.js
@@ -1,17 +1,18 @@
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() {myFunction(navbar, sticky)};
 
-var navbar, sticky = window.onload = function() {
-  setTimeout(
-    function() {
-      // Get the navbar
-      navbar = document.getElementById("navbar")
 
-      // Get the offset position of the navbar
-      sticky = navbar.offsetTop;
-      return navbar, sticky
-    }, 2000);
-};
+// on page reload/refresh
+if (performance.navigation.type == 1) {
+  console.info( "This page is reloaded" );
+  var navbar, sticky = setSticky()
+} else {
+  var navbar, sticky = setSticky()
+}
+// var navbar, sticky = window.onbeforeunload = function() {setSticky()};
+
+
+// var navbar, sticky = window.onload = function() {setSticky()};
 
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
@@ -23,3 +24,15 @@ function myFunction(navbar, sticky) {
     navbar.classList.remove("sticky")
   }
 }
+
+function setSticky() {
+  setTimeout(
+    function() {
+      // Get the navbar
+      navbar = document.getElementById("navbar")
+
+      // Get the offset position of the navbar
+      sticky = navbar.offsetTop;
+      return navbar, sticky
+    }, 2000);
+};
